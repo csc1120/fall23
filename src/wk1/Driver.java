70baccb34384987dbf2816e7d29ff61f9e70f30d
Dr. Taylor <gitlab@taylor.bz>
2023-08-29 10:17:11 -0500

    Finished example that determines the total number of meetings on each weekday.

--- src/wk1/Driver.java
+++ src/wk1/Driver.java
@@ -2,6 +2,8 @@ package wk1;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Driver {
@@ -11,7 +13,19 @@ public class Driver {
         String filename = in.nextLine();
         Path path = Path.of("data/" + filename);
         try (Scanner fin = new Scanner(path)) {
-            System.out.println(fin.nextLine());
+            int[] meetingsPerDayCount = new int[SectionMeeting.DAYS.size()];
+            while (fin.hasNextLine()) {
+                SectionMeeting meeting = new SectionMeeting(fin.nextLine());
+                for (int i = 0; i < meetingsPerDayCount.length; i++) {
+                    if (meeting.meetsOn(SectionMeeting.DAYS.get(i))) {
+                        meetingsPerDayCount[i]++;
+                    }
+                }
+            }
+            System.out.println("Total daily meetings:");
+            for (int i = 0; i < meetingsPerDayCount.length; i++) {
+                System.out.println(SectionMeeting.DAYS.get(i) + ": " + meetingsPerDayCount[i]);
+            }
         } catch (IOException e) {
             System.err.println("Trouble reading file");
         }
--- src/wk1/SectionMeeting.java
+++ src/wk1/SectionMeeting.java
@@ -1,5 +1,7 @@
 package wk1;
 
+import java.util.List;
+
 public class SectionMeeting {
     private String courseCode;
     private String sectionNumber;
@@ -7,20 +9,37 @@ public class SectionMeeting {
     private boolean[] daysOfWeek;
     private int startTime;
     private int endTime;
+    public static final List<String> DAYS = List.of("Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday");
 
     public SectionMeeting(String line) {
         String[] fields = line.split(",");
         final int sectionNumberIndexStart = 10;
+        final int daysOfWeekStartIndex = 2;
+        final int startTimeIndex = daysOfWeekStartIndex + 5;
+        final int endTimeIndex = startTimeIndex + 1;
         courseCode = fields[0].substring(0, sectionNumberIndexStart);
-        sectionNumber = fields[0].substring(sectionNumberIndexStart);
+        sectionNumber = fields[0].substring(sectionNumberIndexStart).trim();
         title = fields[1];
         daysOfWeek = new boolean[5];
-        daysOfWeek[0] = !fields[2].isEmpty();
-        daysOfWeek[1] = !fields[3].isEmpty();
-        daysOfWeek[2] = !fields[4].isEmpty();
-        daysOfWeek[3] = !fields[5].isEmpty();
-        daysOfWeek[4] = !fields[6].isEmpty();
+        for (int i = 0; i < 5; i++) {
+            daysOfWeek[i] = !fields[i + daysOfWeekStartIndex].isEmpty();
+        }
+        String start = fields[startTimeIndex];
+        startTime = Integer.parseInt(start.substring(0, start.indexOf(":")));
+        String end = fields[endTimeIndex];
+        endTime = Integer.parseInt(end.substring(0, end.indexOf(":"))) + 1;
     }
 
+    public boolean meetsAt(String day, int hour) {
+        return (startTime <= hour && hour < endTime) && daysOfWeek[DAYS.indexOf(day)];
+    }
 
+    public boolean meetsOn(String day) {
+        return daysOfWeek[DAYS.indexOf(day)];
+    }
+
+    public String getSectionDescription() {
+        return courseCode + "/" + sectionNumber + ", " + title;
+    }
 }
