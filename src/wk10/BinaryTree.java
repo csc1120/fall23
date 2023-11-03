f365caf014aa14385fcb3a4d1191a0b48e0eb5d4
Dr. Taylor <gitlab@taylor.bz>
2023-11-03 08:53:01 -0500

    Week 10 Lecture 3

--- src/wk10/BinaryTree.java
+++ src/wk10/BinaryTree.java
@@ -1,39 +1,62 @@
 package wk10;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.function.Consumer;
 
 public class BinaryTree<E> {
+
+    private E value;
+    private BinaryTree<E> left;
+    private BinaryTree<E> right;
 
     public BinaryTree() {
 
     }
 
     public BinaryTree(E element) {
-
+        this(element, null, null);
     }
 
     public BinaryTree(E element, BinaryTree<E> left, BinaryTree<E> right) {
-
+        value = element;
+        this.left = left;
+        this.right = right;
     }
 
     public BinaryTree<E> getLeftSubtree() {
-        return null;
+        return left;
     }
 
     public BinaryTree<E> getRightSubtree() {
-        return null;
+        return right;
     }
 
     public boolean isLeaf() {
-        return false;
+        return left == null && right == null;
     }
 
     public void preOrderTraverse(Consumer<E> consumer) {
+        preOrderTraverse(this, consumer);
+    }
 
+    private static <E> void preOrderTraverse(BinaryTree<E> root, Consumer<E> consumer) {
+        consumer.accept(root.value);
+        if (root.left != null) {
+            preOrderTraverse(root.left, consumer);
+        }
+        if (root.right != null) {
+            preOrderTraverse(root.right, consumer);
+        }
     }
 
     @Override
     public String toString() {
-        return null;
+        OutputStream out = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(out);
+        preOrderTraverse(value -> ps.print(value + ", "));
+        String result = out.toString();
+        return '[' + result.substring(0, result.length() - 2) + ']';
     }
 }
--- src/wk10/Driver.java
+++ src/wk10/Driver.java
@@ -4,6 +4,13 @@ import java.util.function.Consumer;
 
 public class Driver {
     public static void main(String[] args) {
+        BinaryTree<String> tree = new BinaryTree<>("opposites",
+                new BinaryTree<>("happy"),
+                new BinaryTree<>("sad"));
+        System.out.println(tree);
+    }
+
+    public static void main2(String[] args) {
         BST<Integer> bst = new BST<>();
         bst.add(5);
         bst.add(4);
